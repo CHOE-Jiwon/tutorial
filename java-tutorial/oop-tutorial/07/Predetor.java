0b01673c9c5aa0f79284a4bd66bcb6bd4a29fda6
ChoeJiwon <cjw20997@gmail.com>
2022-01-10 22:45:04 +0900

    집 가기전 Interface  공부(진행중)

---
+++ java-tutorial/oop-tutorial/07/Predetor.java
@@ -0,0 +1,3 @@
+public interface Predetor {
+    public String getFood();
+}
---
+++ java-tutorial/oop-tutorial/07/Tiger.java
@@ -0,0 +1,5 @@
+public class Tiger extends Animal implements Predetor{
+    public String getFood() {
+        return "Apple";
+    }
+}
